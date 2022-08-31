c51cce40cc5c9ee6ffda8d53624297dd5a5e61e1
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-08-31 20:40:31 +0200

    Good adding images in DOM

--- src/index.js
+++ src/index.js
@@ -9,22 +9,31 @@ const fetchImages = async () => {
   const safeSearch = true;
   const inputValue = input.value.trim();
   const baseUrl = `https://pixabay.com/api/?key=${token}&q=${inputValue}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}&page=1&per_page=40`;
-  const array = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-  ];
 
-  const arrayOfImages = array.map(async array => {
-    const response = await fetch(`${baseUrl}`);
-    return response.json();
-  });
-  // const response = await fetch(`${baseUrl}`);
-  // const arrayOfImages = await response.json();
+  // const arrayOfImages = array.map(async array => {
+  //   const response = await fetch(`${baseUrl}`);
+  //   return response.json();
+  // });
+  const response = await fetch(`${baseUrl}`); 
+  const responseObject = await response.json();
+  const arrayImages = [];
+  responseObject.hits.forEach(async (image) => {
+    arrayImages.push(image);
+    // console.log(image);
+  })
+  // zmienna.hits.forEach(image => {
+  //   console.log(image);
+  // });
+  console.log(typeof arrayImages);
+  console.log(arrayImages);
+  // console.log(responseObject);
+  
+  return arrayImages;
 
-  // return arrayOfImages;
-  const images = await Promise.all(arrayOfImages);
-  console.log(images);
-  return images;
+
+  // const images = await Promise.all(arrayOfImages);
+  // console.log(images);
+  // return images;
 };
 
 const renderImages = images => {
@@ -34,7 +43,7 @@ const renderImages = images => {
   <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes ${image.like}</b>
+      <b>Likes ${image.likes}</b>
     </p>
     <p class="info-item">
       <b>Views ${image.views}</b>
@@ -52,9 +61,9 @@ const renderImages = images => {
   gallery.innerHTML = markup;
 };
 
-fetchImages()
-  .then(images => images)
-  .catch(Error => console.log(Error));
+// fetchImages()
+//   .then(images => images)
+//   .catch(Error => console.log(Error));
 
 inputBtn.addEventListener('click', async event => {
   event.preventDefault();
