a841f729b85738fd419805f3c9a55474caf93e75
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-09-01 19:35:56 +0200

    01.09.2022 end of work today

--- src/index.js
+++ src/index.js
@@ -2,34 +2,35 @@ const input = document.querySelector('.search_input');
 const inputBtn = document.querySelector('.search_btn');
 const gallery = document.querySelector('.gallery');
 
+let page = 1;
+
 const fetchImages = async () => {
   const token = '29544011-a26ad759f9849933fa3601a5e';
   const imageType = 'photo';
   const orientation = 'horizontal';
   const safeSearch = true;
   const inputValue = input.value.trim();
-  const baseUrl = `https://pixabay.com/api/?key=${token}&q=${inputValue}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}&page=1&per_page=40`;
+  const baseUrl = `https://pixabay.com/api/?key=${token}&q=${inputValue}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}&page=${page}&per_page=40`;
 
   // const arrayOfImages = array.map(async array => {
   //   const response = await fetch(`${baseUrl}`);
   //   return response.json();
   // });
-  const response = await fetch(`${baseUrl}`); 
+  const response = await fetch(`${baseUrl}`);
   const responseObject = await response.json();
   const arrayImages = [];
-  responseObject.hits.forEach(async (image) => {
+  responseObject.hits.forEach(async image => {
     arrayImages.push(image);
     // console.log(image);
-  })
+  });
   // zmienna.hits.forEach(image => {
   //   console.log(image);
   // });
-  console.log(typeof arrayImages);
+  console.log(Array.isArray(arrayImages));
   console.log(arrayImages);
   // console.log(responseObject);
-  
-  return arrayImages;
 
+  return arrayImages;
 
   // const images = await Promise.all(arrayOfImages);
   // console.log(images);
@@ -43,16 +44,16 @@ const renderImages = images => {
   <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes ${image.likes}</b>
+      <b>Likes</b> ${image.likes}
     </p>
     <p class="info-item">
-      <b>Views ${image.views}</b>
+      <b>Views</b> ${image.views}
     </p>
     <p class="info-item">
-      <b>Comments ${image.comments}</b>
+      <b>Comments</b> ${image.comments}
     </p>
     <p class="info-item">
-      <b>Downloads ${image.downloads}</b>
+      <b>Downloads</b> ${image.downloads}
     </p>
   </div>
 </div>`
@@ -61,9 +62,12 @@ const renderImages = images => {
   gallery.innerHTML = markup;
 };
 
-// fetchImages()
-//   .then(images => images)
-//   .catch(Error => console.log(Error));
+fetchImages()
+  .then(images => {
+    renderImages(images);
+    page += 1;
+  })
+  .catch(Error => console.log(Error));
 
 inputBtn.addEventListener('click', async event => {
   event.preventDefault();
